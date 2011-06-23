6a541cb23a1737e8fbadddd5b2b8e0c023d4dcd8
Alejandro Valdés <alejandrovaldes@live.com>
2011-06-23 11:39:42 -0500

    agregadas funciones para comprimir video sin block matching

--- ComprimirVideo.cpp
+++ ComprimirVideo.cpp
@@ -1,10 +1,12 @@
 #include "ComprimirVideo.h"
 #include "CompressorFile.h"
 #include "TransCoseno.h"
+#include "LeerImagen.h"
 #include <math.h>
 #include <vector>
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
+#include <cstdio>
 #include <sstream>
 #include <string>
 
@@ -19,20 +21,42 @@ vector<vector<double> > operator-(const vector<vector<double> >& a, const vector
 	return resultado;
 }
 
+vector<vector<double> > operator-(const vector<vector<int> >& a, const vector<vector<int> >& b){
+	int w=a.size(), h=a[0].size();
+	vector<vector<double> > resultado(w, vector<double>(h, 0));
+	for(int i=0;i<w;i++)
+	    for(int j=0;j<h;j++)
+		resultado[i][j]=a[i][j]-b[i][j];
+	return resultado;
+}
+
 ComprimirVideo::ComprimirVideo(string prefijo, int numero_imagenes){
 	this->prefijo=prefijo;
-	IMAGE_NUMBER=numero_imagenes;
-	
+	IMAGE_NUMBER=numero_imagenes;	
 }
-/*
+
 void ComprimirVideo::comprimirImagenes(){
-   int i=0;
-   while(true){
-      ifstream FileIn(prefijo.c_str(), ios::in);
-      if(!FileIn.is_open()) return;
-   }
+	string firstimage=prefijo+"0.pgm";
+	LeerImagen li(firstimage);
+	vector<vector<int> > anterior=li.getMatriz();
+	vector<vector<double> > tuplass=TransCoseno::comprimirImagen(anterior);
+	CompressorFile cf((int)anterior.size(),(int)anterior[0].size(), 8.0, tuplass);
+	cf.CreateFile("comprimida0.cvca");
+	char bff [4];
+	for(int i=1;i<IMAGE_NUMBER;i++){
+		sprintf (bff, "%d", i);	
+		string numero(bff);	
+		string actpath=prefijo+numero+".pgm";
+		LeerImagen li(actpath);
+		vector<vector<int> > actual=li.getMatriz();
+		vector<vector<double> > residual=actual-anterior;
+		vector<vector<double> > tuplass=TransCoseno::comprimirImagen(residual);
+		CompressorFile cf((int)anterior.size(),(int)anterior[0].size(), 8.0, tuplass);
+		cf.CreateFile("comprimida"+numero+".cvca");
+		anterior=actual;
+		actual.clear();
+	}
 }
-*/
 
 void ComprimirVideo::comprimirFotograma(vector<vector<double> >& anterior, vector<vector<double> >& actual, string path){
 	vector<vector<double> > matrizImagen = actual-anterior;
--- LeerImagen.cpp
+++ LeerImagen.cpp
@@ -12,79 +12,50 @@ using namespace std;
 
 
 LeerImagen::LeerImagen(string NameFile){
-	 
-	
-         string index, comment, cols, raws, maxbit;
-	 int Cols, Raws, MaxBit;
-         ifstream FileIn(NameFile.c_str(), ios::in);
-
-         if(!FileIn.is_open()){
-                                  cerr<<"no se pudo abrir el archivo "<<NameFile<<" :("<<endl;
-
-                                  }
-         getline(FileIn, index);
-         getline(FileIn, comment);
-
-         while(comment[0]=='#')
-             getline(FileIn,comment);
-
-         istringstream tokenizer(comment);
-         tokenizer>>cols>>raws;
-         FileIn >> maxbit;
-         assert( maxbit=="255" );
-
-
-        Cols=atoi(cols.c_str());
-        Raws=atoi(raws.c_str());
-        MaxBit=atoi(maxbit.c_str());
-
-	 assert( (Cols%8)==0 );
-         assert( (Raws%8)==0 );
-         //Creation de las matrices
-
-         Matriz.resize(Cols,vector<int>(Raws,0));
-	
-
-       if (index=="P3"){
-
-
-         vector< vector <int> > Red(Cols,vector<int>(Raws,0));
-         vector< vector <int> > Green(Cols,vector<int>(Raws,0));
-         vector< vector <int> > Blue(Cols,vector<int>(Raws,0));
-
+	string index, comment, cols, raws, maxbit;
+	int Cols, Raws, MaxBit;
+    ifstream FileIn(NameFile.c_str(), ios::in);
+    assert(FileIn.is_open());
+    getline(FileIn, index);
+    getline(FileIn, comment);
+    while(comment[0]=='#')
+		getline(FileIn,comment);
+	istringstream tokenizer(comment);
+    tokenizer>>cols>>raws;
+    FileIn >> maxbit;
+    assert( maxbit=="255" );
+    Cols=atoi(cols.c_str());
+    Raws=atoi(raws.c_str());
+    MaxBit=atoi(maxbit.c_str());
+	assert( (Cols%8)==0 );
+    assert( (Raws%8)==0 );
+    //Creation de las matrices
+    Matriz.resize(Cols,vector<int>(Raws,0));
+	if (index=="P3"){
+		vector< vector <int> > Red(Cols,vector<int>(Raws,0));
+        vector< vector <int> > Green(Cols,vector<int>(Raws,0));
+        vector< vector <int> > Blue(Cols,vector<int>(Raws,0));
         for(int i=0;i<Raws;i++){
-             for(int j=0;j<Cols;j++){
-                       FileIn >> Red[j][i];
-                       FileIn >> Green[j][i];
-                       FileIn >> Blue[j][i];
-                         }
-                                     }
-
-
-            for(int i=0;i<Raws;i++){
-                  for(int j=0;j<Cols;j++){
-                      Matriz.at(j).at(i)=RGBtoGray(Red[j][i],Green[j][i],Blue[j][i]);
-
-                                                 }
-                                         }
-
+			for(int j=0;j<Cols;j++){
+				FileIn >> Red[j][i];
+                FileIn >> Green[j][i];
+                FileIn >> Blue[j][i];
             }
-
-     else{
-
-         for(int i=0;i<Raws;i++){
-               for(int j=0;j<Cols;j++){
-                   FileIn >>Matriz[j][i];
-
-                }
-           }
+        }
+		for(int i=0;i<Raws;i++){
+			for(int j=0;j<Cols;j++){
+				Matriz.at(j).at(i)=RGBtoGray(Red[j][i],Green[j][i],Blue[j][i]);
+			}
+		}
+    }
+    else{
+		for(int i=0;i<Raws;i++){
+			for(int j=0;j<Cols;j++){
+				FileIn >>Matriz[j][i];
+            }
+        }
      }
-
      FileIn.close();
-
-    
-
-     
 }
 
 
@@ -94,6 +65,5 @@ int LeerImagen::RGBtoGray(int r, int g, int b){
 
 
 vector< vector<int> > LeerImagen::getMatriz(){
-	
     return Matriz;
 }
